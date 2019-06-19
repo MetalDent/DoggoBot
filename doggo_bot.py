0d43d6678c0ef70febb457bc0b29d740495d7ca6
MetalDent <priyapandya274@gmail.com>
2019-06-19 16:41:49 +0530

    Add code

--- doggo_bot.py
+++ doggo_bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import requests
 import re
 
-def get_url():
+def get_url_imageAPI():
     contents = requests.get('https://random.dog/woof.json').json()	#API
     url = contents['url']
     return url
@@ -11,14 +11,24 @@ def get_image_url():
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
     while file_extension not in allowed_extension:
-        url = get_url()
+        url = get_url_image()
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
+
+def get_url_factAPI():
+	
+
+
 
 def woof(bot, update):	#request for image
     url = get_image_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
+
+def fact(bot, update):	#request for video
+	url = get_fact_url()
+	chat_id = update.message.chat_id
+	bot.send_fact(chat_id=chat_id, fact=url)
 
 def main():
     updater = Updater('723455293:AAGckXg1oR6k61GBWVEJeMM9lk3vjnDBdHY')
